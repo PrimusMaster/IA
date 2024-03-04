23536913d259ac7989da9722bc4c9d1d8669b409
PrimusMaster <supremo_games@hotmail.com>
2024-03-04 01:43:26 -0600

    Update AplicacionGatoAleatorio.py

--- AplicacionGatoAleatorio.py
+++ AplicacionGatoAleatorio.py
@@ -15,3 +15,33 @@ Estadodejuego = [0 , 0 , 0 ,
 #Esta variable marca el numero de espacios disponibles en el juego para saber cuando se llego a un empate
 Espaciosdisponibles = 9
 
+#La funcion imprimir es la que se encarga de darle formato al arreglo del juego para poder ser visible por el jugador
+def Imprimir(EJ):
+    for i in range(0,9): #Este primer ciclo for se encarga de atravesar cada elemento en el arreglo y ver que valor contiene para mostrar una señal    
+        if EJ[i] == 0:
+            print(aux1, end = "")
+            if (i+1) % 3 != 0: #Estos if se encargan de hacer saber a la funcion cuando llego a un elemento 3 y debe de hacer salto de linea, si no, solamente imprimen el objeto
+                print(aux4, end = "")
+            else:
+                print("\n")
+                for j in range(0,3):
+                    print(aux5, end = "")
+                print("\n")
+        elif EJ[i] == 1:
+            print(aux2, end = "")
+            if (i+1) % 3 != 0:
+                print(aux4, end = "")
+            else:
+                print("\n")
+                for j in range(0,3):
+                    print(aux5, end = "")
+                print("\n")
+        elif EJ[i] == 2:
+            print(aux3, end = "")
+            if (i+1) % 3 != 0:
+                print(aux4, end = "")
+            else:
+                print("\n")
+                for j in range(0,3):
+                    print(aux5, end = "")
+                print("\n")
