9c1069f8a6967bdd1ad149da9309da4b57b0c1ef
PrimusMaster <supremo_games@hotmail.com>
2024-03-04 01:42:56 -0600

    Update AplicacionGatoAleatorio.py

--- AplicacionGatoAleatorio.py
+++ AplicacionGatoAleatorio.py
@@ -6,3 +6,12 @@ aux2 = " X "
 aux3 = " O "
 aux4 = "|"
 aux5 = "----"
+
+#Esta variable es la que se usa para representar el juego, el juego se representa el 0 como espacio vacio, el 1 como la x del jugador y el 2 como el O de la computadora
+Estadodejuego = [0 , 0 , 0 , 
+                 0 , 0 , 0 ,
+                 0 , 0 , 0]
+
+#Esta variable marca el numero de espacios disponibles en el juego para saber cuando se llego a un empate
+Espaciosdisponibles = 9
+
